d0b8a16a945292c7efc3fbed40afd0f212f89525
Raji Fathia <fathiaraji4@gmail.com>
2023-08-09 00:40:20 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,27 +2,44 @@
 """
 Using reddit's API
 """
+
 import requests
-after = None
 
+def recurse(subreddit, hot_list=None, after=None):
+    if hot_list is None:
+        hot_list = []
 
-def recurse(subreddit, hot_list=[]):
-    """returning top ten post titles recursively"""
-    global after
-    headers = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {'after': after}
-    result = requests.get(url, params=params, headers=headers,
-                          allow_redirects=False)
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    params = {'limit': 100, 'after': after}
 
-    if result.status_code == 200:
-        data = result.json().get("data").get("after")
-        if data is not None:
-            after = data
-            recurse(subreddit, hot_list)
-        titles = result.json().get("data").get("children")
-        for title in titles:
-            hot_list.append(title.get("data").get("title"))
-        return hot_list
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        articles = data.get('data', {}).get('children', [])
+
+        if not articles:
+            return None
+
+        for article in articles:
+            title = article.get('data', {}).get('title', '')
+            hot_list.append(title)
+
+        after = data.get('data', {}).get('after', None)
+
+        if after:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+    elif response.status_code == 404:
+        return None
     else:
-        return (None)
+        raise Exception(f'Request failed with status code: {response.status_code}')
+
+# Example usage:
+result = recurse('programming')
+if result is not None:
+    print(len(result))
+else:
+    print("None")
